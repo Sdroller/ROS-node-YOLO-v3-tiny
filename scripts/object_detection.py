77eca4be11658513d9a97fb63a4706b3934d61cd
Shreeyak <ss.shrek7@gmail.com>
2018-10-14 11:57:08 +0000

    fixed error in yolov3

--- scripts/object_detection.py
+++ scripts/object_detection.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import time
+import sys
 import rospy
 from std_msgs.msg import String
 from cv_bridge import CvBridge
@@ -35,12 +36,12 @@ class ObjectDetection:
 
         # Subscribers
         self.topic_camera_input = self.config['topics']['topic_camera_input']
-        self.topic_yolo_output_img = self.config['topics']['topic_yolo_output_img']
-	    # https://stackoverflow.com/questions/26415699/ros-subscriber-not-up-to-date/29160379#29160379
+	# https://stackoverflow.com/questions/26415699/ros-subscriber-not-up-to-date/29160379#29160379
         rospy.Subscriber(self.topic_camera_input, Image, self.classify, queue_size=1, buff_size=16777216)
 
         # Publish the image with detection boxes drawn
-        self.img_pub = rospy.Publisher(self.topic_yolo_output_img, Image, queue_size=1)
+        self.topic_yolo_output_img = self.config['topics']['topic_yolo_output_img']
+	self.img_pub = rospy.Publisher(self.topic_yolo_output_img, Image, queue_size=1)
 
 
     def classify(self, image):
