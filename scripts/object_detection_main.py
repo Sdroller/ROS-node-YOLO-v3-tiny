135a73b5fb918c321931ff0091b733deece95462
Shreeyak <ss.shrek7@gmail.com>
2018-10-24 15:16:29 +0530

    yolo can now publish bbox. Modified structure of launchfile

--- scripts/object_detection_main.py
+++ scripts/object_detection_main.py
@@ -16,28 +16,18 @@ import yaml
 from cv_bridge import CvBridge
 from object_detection.msg import bbox_array, bbox
 
-class ObjectDetection:
+class ObjectDetection():
     yolo = None
     config = None
     bridge = None
-    topic_camera_input = '/123fcv_camera/image_raw'
-    topic_yolo_output_img = '123yolo_output_img'
 
     def __init__(self):
+        self.topic_camera_input = '/camera/image_raw'
+        self.topic_yolo_output_img = 'labelled_img'
+        self.topic_yolo_bbox_array = 'bbox_arr'
+
         rospack = rospkg.RosPack()
         path = rospack.get_path('object_detection')
-
-        # Test custom msg
-        msg_yolov3 = bbox()
-        msg_yolov3_arr = bbox_array()
-        msg_yolov3.Class = 'yo!'
-        msg_yolov3.prob = 0.5
-        msg_yolov3.xmin = 4
-        msg_yolov3.ymin = 3
-        msg_yolov3.xmax = 2
-        msg_yolov3.ymax = 1
-        msg_yolov3_arr.bboxes.append(msg_yolov3)
-	print(msg_yolov3_arr)
 
         # Load config
         object_detection_config = rospy.get_param("/object_detection_config")
@@ -47,14 +37,12 @@ class ObjectDetection:
         self.bridge = CvBridge()
 
         # Subscribers
-        self.topic_camera_input = self.config['topics']['topic_camera_input']
-	    # https://stackoverflow.com/questions/26415699/ros-subscriber-not-up-to-date/29160379#29160379
+        # https://stackoverflow.com/questions/26415699/ros-subscriber-not-up-to-date/29160379#29160379
         rospy.Subscriber(self.topic_camera_input, Image, self.classify, queue_size=1, buff_size=16777216)
 
         # Publish the image with detection boxes drawn
-        self.topic_yolo_output_img = self.config['topics']['topic_yolo_output_img']
-	self.img_pub = rospy.Publisher(self.topic_yolo_output_img, Image, queue_size=1)
-
+        self.img_pub = rospy.Publisher(self.topic_yolo_output_img, Image, queue_size=1)
+        self.bbox_pub = rospy.Publisher(self.topic_yolo_bbox_array, bbox_array, queue_size=1)
 
     def classify(self, image):
         img = self.bridge.imgmsg_to_cv2(image, "bgr8")
@@ -64,6 +52,21 @@ class ObjectDetection:
         # Publish output Img
         img_ret_ros = self.bridge.cv2_to_imgmsg(img_ret, encoding="rgb8")
         self.img_pub.publish(img_ret_ros)
+
+        # Publish Bounding Boxes and Probabilities
+        msg_yolov3_arr = bbox_array()
+        for _box, _score, _class in zip(boxes, scores, classes):
+            msg_yolov3 = bbox()
+            msg_yolov3.Class = str(_class)
+            msg_yolov3.prob = _score
+            msg_yolov3.xmin = _box[0]
+            msg_yolov3.ymin = _box[1]
+            msg_yolov3.xmax = _box[2]
+            msg_yolov3.ymax = _box[3]
+            msg_yolov3_arr.bboxes.append(msg_yolov3)
+
+        self.bbox_pub.publish(msg_yolov3_arr)
+
 
 
 def main(args):
